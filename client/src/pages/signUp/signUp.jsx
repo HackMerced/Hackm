c11512bc2d0a49eaf2d6ad713bc49dbef8599951
Shivanshu Gupta <sgupta35@ucmerced.edu>
2019-06-09 17:32:51 -0700

    added respective route destinations and synchronized convention through code

---
+++ client/src/pages/signUp/signUp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './signUp.css';
+
+function SignUp() {
+  return (
+  	<div className="App-sign-up" id="sign-up">
+	  	<div className="container">
+		  	<div className="signup">
+		  	<h1>Sign Up</h1>
+		  	</div>
+
+	  	</div>
+  	</div>
+  );
+}
+export default signUp;
+
+
+
---
+++ client/src/pages/volDash/volDash.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './volDash.css';
+
+function Login() {
+  return (
+  	<div className="App-Vol" id="Vol-Dash">
+	  	<div className="container">
+		  	<div className="volDash">
+		  	<h1>Volunteer Dashboard</h1>
+		  	</div>
+
+	  	</div>
+  	</div>
+  );
+}
+export default volDash;
+
+
+
