854f72ffd45270533164686d79cd249dc17f4d7d
AndrewCNg24 <ang26@ucmerced.edu>
2019-06-22 14:00:10 -0700

    Added multiple people

--- server/api/routes.js
+++ server/api/routes.js
@@ -10,8 +10,16 @@ router.post('/api/mailing', async (req, res) => { };
 
 router.post('/api/hacker', async (req, res) => { };
 
+router.post('/api/hackers', async (req, res) => { };
+
 router.post('/api/volunteer', async (req, res) => { };
+
+router.post('/api/volunteers', async (req, res) => { };
 
 router.post('/api/mentor', async (req, res) => { };
 
+router.post('/api/mentors', async (req, res) => { };
+
 router.post('/api/sponsor', async (req, res) => { };
+
+router.post('/api/sponsors', async (req, res) => { };
