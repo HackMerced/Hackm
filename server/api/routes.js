cc39e06889b34552fb9f67db313a36f485b33599
Sidharth Babu <sidharthbabukish121@gmail.com>
2019-06-26 21:08:18 -0700

    Sid fixed Andrew's error in  routes.js

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,43 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/api/mailing/:id', async (req, res) => { };
+router.post('/api/mailing/:id', async (req, res) => { });
 
-router.post('/api/mailing-list', async (req, res) => { };
+router.post('/api/mailing-list', async (req, res) => { });
 
-router.post('/api/hacker/:id', async (req, res) => { };
+router.post('/api/hacker/:id', async (req, res) => { });
 
-router.post('/api/hackers', async (req, res) => { };
+router.post('/api/hackers', async (req, res) => { });
 
-router.post('/api/volunteer/:id', async (req, res) => { };
+router.post('/api/volunteer/:id', async (req, res) => { });
 
-router.post('/api/volunteers', async (req, res) => { };
+router.post('/api/volunteers', async (req, res) => { });
 
-router.post('/api/mentor/:id', async (req, res) => { };
+router.post('/api/mentor/:id', async (req, res) => { });
 
-router.post('/api/mentors', async (req, res) => { };
+router.post('/api/mentors', async (req, res) => { });
 
-router.post('/api/sponsor/:id', async (req, res) => { };
+router.post('/api/sponsor/:id', async (req, res) => { });
 
-router.post('/api/sponsors', async (req, res) => { };
+router.post('/api/sponsors', async (req, res) => { });
 
 
-router.get('/api/mailing/:id', async (req, res) => { };
+router.get('/api/mailing/:id', async (req, res) => { });
 
-router.get('/api/mailing-list', async (req, res) => { };
+router.get('/api/mailing-list', async (req, res) => { });
 
-router.get('/api/hacker/:id', async (req, res) => { };
+router.get('/api/hacker/:id', async (req, res) => { });
 
-router.get('/api/hackers', async (req, res) => { };
+router.get('/api/hackers', async (req, res) => { });
 
-router.get('/api/volunteer/:id', async (req, res) => { };
+router.get('/api/volunteer/:id', async (req, res) => { });
 
-router.get('/api/volunteers', async (req, res) => { };
+router.get('/api/volunteers', async (req, res) => { });
 
-router.get('/api/mentor/:id', async (req, res) => { };
+router.get('/api/mentor/:id', async (req, res) => { });
 
-router.get('/api/mentors', async (req, res) => { };
+router.get('/api/mentors', async (req, res) => { });
 
-router.get('/api/sponsor/:id', async (req, res) => { };
+router.get('/api/sponsor/:id', async (req, res) => { });
 
-router.get('/api/sponsors', async (req, res) => { };
+router.get('/api/sponsors', async (req, res) => { });
