7f0be0aa4fe2af509325d9d50d04612dd4197c9e
Shivanshu Gupta <sgupta35@ucmerced.edu>
2019-06-28 15:53:56 -0700

    changed needed files from stateless to stateful

--- client/src/pages/login/adminDash/admin.jsx
+++ client/src/pages/login/adminDash/admin.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './admin.css';
 
-const Admin = () => {
-  return (
-  	<div className="App-Admin">  {/* This code basically puts "Admin Dash"*/}
-	  	<section className="container">			{/* in the middle of the page */}
-		  	<article className="admin">
-		  	<h1>Admin Dash</h1> 
-		  	</article>
-	  	</section>
-  	</div>
-  );
+class Admin extends React.Component {
+	render(){
+	  return (
+	  	<div className="App-Admin">  {/* This code basically puts "Admin Dash"*/}
+		  	<section className="container">			{/* in the middle of the page */}
+			  	<article className="admin">
+			  	<h1>Admin Dash</h1> 
+			  	</article>
+		  	</section>
+	  	</div>
+	  );
+	}
 }
 export default Admin;
 
