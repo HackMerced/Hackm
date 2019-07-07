45624fc1cd4eafd10127ca109641b7460c19e248
Shivanshu Gupta <sgupta35@ucmerced.edu>
2019-07-06 20:54:19 -0700

    created a basic login page

--- client/src/pages/login/mainLogin/login.jsx
+++ client/src/pages/login/mainLogin/login.jsx
@@ -5,12 +5,44 @@ import { Link } from 'react-router-dom' //Links Library from React Router
 class Login extends React.Component { 
 	render() {
 	  return (
-	  	<div className="App-login">   {/* This code basically puts "This is the login screen" in big words*/}
-		  	<section className="container">			 {/* Along with tester links at the bottom to each respective dash */}
-			  	<article className="login">
-			  		<h1>This is the login screen</h1>   {/* Big words of "this is the login screen" in middle of page */}
-			  	</article>
-		  	</section>
+	  	<div className="App-login">   
+		  	<article className="center-the-login">
+			  		<section className = "login-title">
+			  			<h2>Login</h2>
+			  		</section>
+			  		<main>
+				  		<form>
+					  		<section className="email"> 	{/* Email Section */}
+					  			<p>Email</p>
+					  		</section>
+
+					  		<input type="text" style={{width:200}} name="email"/>		{/* Email input box */}
+
+					  		<section className="pass">		{/* Password Section */}
+					  			<p>Password</p>
+					  		</section>
+
+					  		<input type="password" id="pass" style={{width:200}} name="password" minlength="8" required/>		{/* Hidden input Password */}
+						</form>
+						 <br></br>
+						 <section  className="logged-In"> 		{/* Keep me Logged in */}
+							<input type="checkbox" name="logged-in?"/>Keep me logged in
+						</section>
+						<br></br>
+						<br></br>
+						<input type="submit" style={{width:210}} value="Lets Go!"/>		{/* Submit */}
+			  			<br></br>
+			  			<br></br>
+			  		</main>
+			  		<section className="signUp-text">		{/* Create an Account Section*/}
+			  			<span>
+			  				Don’t have an account?
+			  			</span>
+			  			<span>
+			  				<Link class = "signUp-link" to='/signup' title= "Join Us!">Sign Up!</Link>
+			  			</span>
+			  		</section>
+		  	</article>
 	  	</div>
 	  );
 	}
