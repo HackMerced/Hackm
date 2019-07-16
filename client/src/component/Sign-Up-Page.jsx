491146944df681591d3e8dc668ae9ae90cdd9cff
Andrew <andrewc.ng24@gmail.com>
2019-07-16 15:38:14 -0700

    Closed input tags and routes

--- client/src/component/Sign-Up-Page.jsx
+++ client/src/component/Sign-Up-Page.jsx
@@ -32,93 +32,93 @@ class SignUpHacker extends Component {
     {/* First Name */}
     <div>
         <label>First Name</label>
-        <input type="text" name="FirstName" ref="name" placeholder="First Name" />
+        <input type="text" name="FirstName" ref="name" placeholder="First Name"></input>
     </div>
 {/* Last Name */}
     <div>
         <label>Last Name</label>
-        <input type="text" name="LastName" ref="name" placeholder="Last Name" />
+        <input type="text" name="LastName" ref="name" placeholder="Last Name"></input>
     </div>
     {/* Email */}
     <div>
         <label>Email</label>
-        <input type="email" name="myEmail" ref="name" placeholder="something@example.com" />
+        <input type="email" name="myEmail" ref="name" placeholder="something@example.com"></input>
     </div>
     {/* Phone Number */}
     <div>
         <label>Phone Number</label>
-        <input type="number" name="phoneNumber" ref="name" placeholder="My Phone number is..." />
+        <input type="number" name="phoneNumber" ref="name" placeholder="My Phone number is..."></input>
     </div>
     {/* University */}
     <div>
         <label>University</label>
-        <input type="text" name="University" ref="name" placeholder="University of..." />
+        <input type="text" name="University" ref="name" placeholder="University of..."></input>
     </div>
     {/* High School */}
     <div>
         <label>High School</label>
-        <input type="text" name="HighSchool" ref="name" placeholder="Something High School" />
+        <input type="text" name="HighSchool" ref="name" placeholder="Something High School"></input>
     </div>
     {/* Birthday npm install react-datepicker --save*/}
     <div>
         <label>Date of Birth</label>
-        <input type="date" name="Birthday" ref="name" placeholder="Last Name" />
+        <input type="date" name="Birthday" ref="name" placeholder="Last Name"></input>
     </div>
     {/* Gender */}
     <div>
         <label>Gender</label>
-        <input type="text" name="Gender" ref="name" placeholder="Male/Female/Other" />
+        <input type="text" name="Gender" ref="name" placeholder="Male/Female/Other"></input>
     </div>
     {/* Race/Ethnicity */}
     <div>
         <label>Race/Ethnicity</label>
-        <input type="text" name="Ethnicity" ref="name" placeholder="Race" />
+        <input type="text" name="Ethnicity" ref="name" placeholder="Race"></input>
     </div>
     {/* Major */}
     <div>
         <label>College Major</label>
-        <input type="text" name="CollegeMajor" ref="name" placeholder="Major" />
+        <input type="text" name="CollegeMajor" ref="name" placeholder="Major"></input>
     </div>
     {/* College Year */}
     <div>
         <label>Year In Colleg</label>
-        <input type="text" name="CollegeYear" ref="name" placeholder="1st..." />
+        <input type="text" name="CollegeYear" ref="name" placeholder="1st..."></input>
     </div>
     {/* T-Shirt Size */}
     <div>
         <label>T-Shirt Size</label>
-        <input type="text" name="ShirtSize" ref="name" placeholder="S..." />
+        <input type="text" name="ShirtSize" ref="name" placeholder="S..."></input>
     </div>
     {/* Dietary Restrictions */}
     <div>
         <label>Dietary Restrictions</label>
-        <input type="text" name="myDiet" ref="name" placeholder="No meat, no animal produce" />
+        <input type="text" name="myDiet" ref="name" placeholder="No meat, no animal produce"></input>
     </div>
     {/* Allergies */}
     <div>
         <label>Allergies</label>
-        <input type="text" name="myAllergies" ref="name" placeholder="Grass..." />
+        <input type="text" name="myAllergies" ref="name" placeholder="Grass..."></input>
     </div>
     {/* Resume */}
     <div>
         <label>Last Name</label>
-        <input type="file" name="resume" ref="name" placeholder="" />
+        <input type="file" name="resume" ref="name" placeholder=""></input>
         <button type="button" onClick={}>Upload</button>
     </div>
     {/* GitHub */}
     <div>
         <label>Last Name</label>
-        <input type="url" name="gitHub" ref="name" placeholder="github.com/yourusernamehere" />
+        <input type="url" name="gitHub" ref="name" placeholder="github.com/yourusernamehere"></input>
     </div>
     {/* LinkedIn */}
     <div>
         <label>LinkedIn</label>
-        <input type="text" name="LinkedIn" ref="name" placeholder="LinkedIn" />
+        <input type="text" name="LinkedIn" ref="name" placeholder="LinkedIn"></input>
     </div>
     {/* Devpost */}
     <div>
         <label>Devpost</label>
-        <input type="text" name="Devpost" ref="name" placeholder="Idk" />
+        <input type="text" name="Devpost" ref="name" placeholder="Idk"></input>
     </div>
     {/* Apply Button */}
     <button>Apply</button>
