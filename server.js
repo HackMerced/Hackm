d7b50a8027ade6856bdef236f39ab2cbf485fffc
Adrian Darian <adarian@ucmerced.edu>
2019-09-23 02:01:06 -0700

    working mongodb connection

--- server.js
+++ server.js
@@ -1,25 +1,69 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+require("dotenv").config();
 
-const hackers = require('./src/routes/hackers');
-const mailing = require('./src/routes/mailing');
-const mentors = require('./src/routes/mentors');
-const sponsors = require('./src/routes/sponsors');
-const volunteers = require('./src/routes/volunteers');
+const routes = require("./src/routes");
+const models = require("./src/models");
+const connectDb = require("./src/models").connectDb;
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
-app.use('/api', hackers, mailing, mentors, sponsors, volunteers);
+app.use(morgan("tiny"));
+app.use(
+  "/api",
+  routes.hackers,
+  routes.mailing,
+  routes.mentors,
+  routes.sponsors,
+  routes.volunteers
+);
 
-app.use(express.static(path.join(__dirname, '/client/build/')))
+app.use(express.static(path.join(__dirname, "/client/build/")));
 
-app.listen(port, async () => {
-   console.log(`Initiating server on ${port}`)
+const eraseDatabaseOnSync = false;
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({}),
+    ]);
+    
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+  );
 });
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'rwieruch',
+  });
+  const user2 = new models.User({
+    username: 'ddavids',
+  });
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id,
+  });
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id,
+  });
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id,
+  });
+
+  await message1.save();
+  await message2.save();
+  await message3.save();
+  await user1.save();
+  await user2.save();
+};
--- src/routes/hackers.js
+++ src/routes/hackers.js
@@ -1,11 +1,26 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const hacker = require("../models").models.Hacker;
 
-router.get('/hacker/:id', async (req, res) => { });
+router.get("/hacker/:id", async (req, res) => {});
 
-router.get('/hackers', async (req, res) => { res.send('hello') });
+router.get("/hackers", async (req, res) => {
+  res.send("hello");
+});
 
-router.post('/hacker/:id', async (req, res) => { });
+router.post("/hacker/:id", async (req, res) => {});
 
-router.post('/hackers', async (req, res) => { });
+router.post("/hackers", async (req, res) => {
+  console.log('re: ', req);
+  var hackers = [{ username: "Jean" }, { username: "Luc" }];
+  hacker.insertMany(hackers, (error, docs) => {
+    if (error) {
+      res.send(error);
+    }
+
+    if (docs) {
+      res.send(docs);
+    }
+  });
+});
 
 module.exports = router;
