382b0e055183be6780761a72b91d8b8a8b5f1479
Adrian Darian <adarian@ucmerced.edu>
2019-09-29 23:49:17 -0700

    initial hacker schema

--- src/models/hacker.js
+++ src/models/hacker.js
@@ -1,24 +1,106 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const hackerSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
+  firstName: {
+    type: String
   },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String,
+    index: {
+      unique: true
+    },
+    validate: {
+      validator(e) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          e
+        );
+      },
+      message: "{VALUE} is not a valid email!"
+    },
+    required: [true, "User email required"]
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator(e) {
+        return /^[+0-9\s]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(e);
+      },
+      message: "{VALUE} is not a valid phone number!"
+    },
+    required: [true, "User phone number required"]
+  },
+  school: {
+    type: String,
+    required: [true, "School required"]
+  },
+  age: {
+    type: Number,
+    min: [13, "Too young"],
+    max: [30, "Too old"],
+    required: [true, "Age required"]
+  },
+  gender: {
+    type: String,
+    enum: [
+      "male",
+      "female",
+      "lesbian",
+      "gay",
+      "bisexual",
+      "transgender",
+      "queer",
+      "neither"
+    ],
+    required: [true, "Gender required"]
+  },
+  skills: [String],
+  interests: [String],
+  programmingLanguages: [String],
+  dietaryRestrictions: [String],
+  allergies: [String],
+  specialNeeds: String,
+  shirtSize: {
+    type: String,
+    required: [true, "Shirt size required"]
+  },
+  previousHackathons: {
+    type: Number,
+    required: [true, "Number of previous hackathons required"]
+  },
+  haveTeam: {
+    type: Boolean,
+    required: [true, "Please specify if you have a team"]
+  },
+  teamEmails: [String],
+  linkedin: String,
+  github: String,
+  otherSites: String,
+  resume: String,
+  applicationStatus: {
+    type: String,
+    default: "submitted",
+    enum: ["submitted", "accepted", "declined", "waitListed", "checkedIn"],
+    required: [true, "Application status required"]
+  },
+
+  privileges: {
+    type: String,
+    required: [true, "User privileges required"]
+  },
+  qr: {
+    type: String,
+    index: {
+      unique: true,
+      sparse: true
+    }
+  }
 });
 
-// hackerSchema.statics.findByLogin = async function (login) {
-//   let user = await this.findOne({
-//     username: login,
-//   });
-//   if (!user) {
-//     user = await this.findOne({ email: login });
-//   }
-//   return user;
-// };
+hackerSchema.pre("save", function lowerCaseFields(next) {
+  this.firstName = this.firstName.toLowerCase();
+});
 
-// hackerSchema.pre('remove', function(next) {
-//   this.model('Team').deleteMany({ hacker: this._id }, next);
-// });
-
-module.exports = mongoose.model('Hacker', hackerSchema);
+module.exports = mongoose.model("Hacker", hackerSchema);
