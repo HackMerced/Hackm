a1d4fcfc2d02d5fac7e8db2362bbe800598d3f36
root <root@Lorenzo_Computer.localdomain>
2019-10-01 16:44:51 -0700

    changed the component to client side and made CSS file as well

---
+++ client/src/component/Schedule/Schedule.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Schedule extends React.Component {
+  render() {
+    return (
+        <table id="simple-board">
+        <tbody>
+          <tr id="row0">
+            <td id="cell0-0"></td>
+            <td id="cell0-1"></td>
+            <td id="cell0-2"></td>
+          </tr>
+          <tr id="row1">
+            <td id="cell1-0"></td>
+            <td id="cell1-1"></td>
+            <td id="cell1-2"></td>
+          </tr>
+          <tr id="row2">
+            <td id="cell2-0"></td>
+            <td id="cell2-1"></td>
+            <td id="cell2-2"></td>
+          </tr>
+        </tbody>
+      </table>
+        );
+    }
+}
