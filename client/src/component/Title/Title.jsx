803f2b305d2cde2ab0f6cdff36f5a55203807da5
Busher <bridibusher@gmail.com>
2019-10-15 14:43:04 -0700

    Added email sign up modal

--- client/src/component/Title/Title.jsx
+++ client/src/component/Title/Title.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import 'font-awesome/css/font-awesome.min.css'
 import './Title.css'
 
-const Title = () =>
-{
-    return(
-        <article className = "Title" id = "Header">
-            <section className = "container">
+const Title = () => {
+    return (
+        <article className="Title" id="Header">
+            <section className="container">
                 <header>HACKMERCED V</header>
                 <div>20/20/2020</div>
-                <button type = "button">Join E-Mail List</button>
-                
+
+                <div class="box">
+                    <a class="button" href="#popup1">Join the E-mail list</a>
+                </div>
+
+                <div id="popup1" class="overlay">
+                    <div class="popup">
+            <h2>Join our E-Mail list</h2>
+            <form>
+                 <input type = "text" id = "name" placeholder = "Your Name"></input>
+                 <input type = "text" id = "email" placeholder = "Email"></input>
+                 <button type = "submit" id = "sendEmail">submit</button>
+            </form>
+                        <a class="close" href="#">&times;</a>
+
+                    </div>
+                </div>
             </section>
         </article>
     )
