2953884e5349bae7a4604e7a25388d5c59dcdcab
Manjot Singh <msingh795617@gmail.com>
2019-10-16 23:20:54 -0700

    creating the class and funtion

---
+++ client/src/component/contact-us/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactUs extends React.Component {
+    render() {
+        return (
+            <article className="App-con" id="Contact-Us"></article>
+            //all that i have atm
+
+
+            
+        )
+    }
+}
