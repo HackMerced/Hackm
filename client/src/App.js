219e360921dae004c8677d705854a7c1b178501a
Tuzette <tavu2013@gmail.com>
2019-10-21 14:55:10 -0700

    Fixed bugs for banner

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Footer from './component//Footer/Footer.jsx';
 import Main from './component/Main/Main.jsx'
 import NavigationBar from './component/Header/Navigation-Bar.jsx'
-import DayBanner from './component/Header/Day-Banner.jsx';
 
 const App = () => {
   return (
     <React.Fragment> 		{/* This allows children, and removal of unnecessary <div> wrapper */}
       <NavigationBar />
-      <DayBanner />
+
       <Main /> 		{/* Link to React Router */}
       <Footer />	{/* Link to Footer.js */}
     </React.Fragment>
