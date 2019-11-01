064a7212187c49848913dbb119de03e983cfc2ca
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2019-11-01 15:38:24 -0700

    added four more sponsor logos

--- client/src/component/Sponsors/sponsors-logos.jsx
+++ client/src/component/Sponsors/sponsors-logos.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './sponsors-logos.css';
 import ASUCM from "../../assests/asucm_logo.png";
-import password from "../../assests/1password.png";
+import password from "../../assests/dotcom white mono.png";
 import DigitalOcean from "../../assests/DO_Logo_Vertical_White.png";
 import sketch from "../../assests/sketch-logo-dark-transparent@2x.png";
-import roche from "../../assests/Roche_Logo.svg"
+import roche from "../../assests/Roche_Logo.svg";
+import balsamiq from "../../assests/balsamiq-logo-noborder-print.png";
+import stickeryou from "../../assests/stickeryou_logo.png";
+import jetbrains from "../../assests/jetbrains.png";
+import makeschool from "../../assests/makeschool_logo.png";
 //inserted images by moving from file to file
 
 class Sponsors extends React.Component {
@@ -12,7 +16,7 @@ class Sponsors extends React.Component {
         return( 
             <article className = "App-Sponsors" id= "Sponsors">
                  <div className="sponsoring"> {/*sponsoring section*/}
-                <font size = "6" color ="white"> {/* title of Sponsors*/}
+                <font size = "6" color ="white" > {/* title of Sponsors*/}
               <p class = "right"> SPONSORS </p>
                 </font>
 
@@ -22,31 +26,55 @@ class Sponsors extends React.Component {
                 <img className="asucm" src={ASUCM} alt="ASUCM" /> {/*ASUCM logo and link*/}
                  </a> 
                  </div>
-            <div className = "column">
+            <div className = "top">
             <p><a href = "https://digitalocean.com/"> {/*goes to digital ocean site*/}
                  <img className = "digitalocean" src={DigitalOcean} alt="Digital Ocean" /> {/*Digital Ocean logo and link*/}
             </a></p>
             </div>
 
-            <div className = "column">
+            <div className = "top">
              <a href = "https://roche.com/" > {/*goes to roche site*/}
                  <img className = "roche" src= {roche} alt="Roche"/> {/*Roche logo and link*/}
                  </a>
                  </div>
             </div>
 
-            <div className = "bottom"> {/*bottom row for the display*/}
+            <div className = "2row"> {/*bottom row for the display*/}
                 <div className= "password">
             <a href= "https://1password.com/" > {/*goes to 1 password site*/}
                 <img className= "password" src = {password} alt="1Password"/> {/*1 paswword logo and link*/}
                 </a>
+                
+                <div className = "middle">
+                    <a href = "https://www.makeschool.com/">
+                        <img className = "makeschool" src = {makeschool} alt = "Make School" />
+                    </a>
+                </div>
+                <div className = "middle">
+                    <a href = "https://www.stickeryou.com/">
+                        <img className = "stickeryou" src = {stickeryou} alt = "Sticker You" />
+                    </a>
+                </div>
                 </div>
             
-                <div className= "bottom">
+                <div className= "3row">
             <a href = "https://www.sketch.com/"> {/*goes to sketch site*/}
                  <img className = "sketch" src = {sketch} alt="Sketch" /> {/*Sketch logo and link*/}
             </a>
-                         </div>
+               
+                 <div className = "bottom">
+                <a href = "https://balsamiq.com/">
+                        <img className = "balsamiq" src = {balsamiq} alt= "Balsamiq" />
+            </a>
+                    </div>
+              
+                <div className = "bottom">
+                    <a href = "https://www.jetbrains.com/">
+                        <img className = "jetbrains" src = {jetbrains} alt = "Jetbrains" />
+                    </a>
+                </div>
+                </div>
+                      
                     </div>       
                 </div>
              </article>
