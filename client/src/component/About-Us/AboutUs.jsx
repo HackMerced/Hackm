4e8ba3b820fb9a53a42515dc7c91c08aa0ebdf3b
Shivanshu Gupta <sgupta35@ucmerced.edu>
2019-11-05 01:26:36 -0800

    added an About Us section

---
+++ client/src/component/About-Us/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'font-awesome/css/font-awesome.min.css'
+import './aboutUs.css'
+
+class AboutUs extends React.Component {
+    render() {
+        return (
+            <article className="App-AboutUs" id="about-us">
+                <div className="text">
+                  <div className="AboutUs-info">
+                    <h3>What we do</h3>
+                    <p>
+                      HackMerced is a 36-hour annual programming competition that occurs
+                      at the University of California, Merced and is open to students
+                      from all over the world. During the event, participants will
+                      collaborate in teams and attend workshops to learn about new
+                      technologies.
+                    </p>
+                  </div>
+                  <div className="AboutUs-goal">
+                    <h3>Why we do it</h3>
+                    <p>
+                      We aim to create a collaborative, interdisciplinary event that
+                      brings together students from all universities and prospective
+                      sponsors to see the innovation and creativity culminating within
+                      the San Joaquin Valley.
+                    </p>
+                  </div>
+                </div>
+            </article>
+        )
+    }
+}
+export default AboutUs;
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from '../../logo.svg';
 import './home.css'; //import css for this page
+import AboutUs from '../../component/About-Us/AboutUs.jsx'
 
 class Home extends React.Component {   // This page is the exact same page as the original example
   render() {
     return (
+      <div>
       <article className="Home">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,6 +22,10 @@ class Home extends React.Component {   // This page is the exact same page as th
             Learn React
           </a>
       </article>
+      <React.Fragment>    {/* This allows children, and removal of unnecessary <div> wrapper */}
+        <AboutUs />
+    </React.Fragment>
+    </div>
       );
   }
 }
