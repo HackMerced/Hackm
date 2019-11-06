0eab802868323d2dfda1ed08c941e21ef4e6a47e
Lorenzo Scaturchio <lorenzosca7@gmail.com>
2019-11-05 23:50:40 -0800

    Made font bigger, implemented event times (Hack'm)

--- client/src/component/Schedule/Schedule.jsx
+++ client/src/component/Schedule/Schedule.jsx
@@ -11,57 +11,30 @@ class Schedule extends React.Component {
             <tr>
               <th>Event</th>
               <th>Time</th>
-              <th>Location</th>
             </tr>
             <tr>
-              <td>Introductory Ceremony</td>
-              <td>8:00 PM - 9:00 PM</td>
-              <td>COB 102</td>
+              <td>Opening Ceremony</td>
+              <td>8:00 AM</td>
             </tr>
             <tr>
-              <td>Team Mixer</td>
-              <td>9:00 PM - 10:30 PM</td>
-              <td>COB 102</td>
-            </tr>
-            <tr>
-              <td>Dinner</td>
-              <td>9:00 PM - 10:00 PM</td>
-              <td>Outside</td>
-            </tr>
-            <tr>
-              <td>Python Flask Workshop</td>
-              <td>11:00 PM - 12:30 AM</td>
-              <td>SSB 170</td>
-            </tr>
-            <tr>
-              <td>React.js Workshop</td>
-              <td>7:00 AM - 9:00 AM</td>
-              <td>COB2 130</td>
-            </tr>
-            <tr>
-              <td>Breakfast</td>
-              <td>8:00 AM - 9:00 AM</td>
-              <td>COB 102</td>
-            </tr>
-            <tr>
-              <td>Cupstack</td>
-              <td>11:00 AM - 12:00 PM</td>
-              <td>SSB 130</td>
+              <td>Hacking Begins</td>
+              <td>9:00 AM</td>
             </tr>
             <tr>
               <td>Lunch</td>
-              <td>1:00 PM - 3:00 PM</td>
-              <td>COB 102</td>
+              <td>12:00 AM</td>
             </tr>
             <tr>
-              <td>Creating Alexa App with AWS Tools</td>
-              <td>5:00 PM - 7:00 PM</td>
-              <td>SSB 171</td>
+              <td>Dinner</td>
+              <td>5:00 PM</td>
+            </tr>
+            <tr>
+              <td>Hacking Ends</td>
+              <td>9:00 PM</td>
             </tr>
             <tr>
               <td>Closing Ceremony</td>
-              <td>11:00 AM - 12:00 PM</td>
-              <td>COB 102</td>
+              <td>9:00 PM - 10:00 PM</td>
             </tr>
           </table>
         </div>
