5c927c93d379e071300c0bea9fbb3adf6224a730
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2019-11-07 16:27:57 -0800

    changed classname

--- client/src/component/Sponsors/sponsors-logos.jsx
+++ client/src/component/Sponsors/sponsors-logos.jsx
@@ -18,69 +18,69 @@ class Sponsors extends React.Component {
             <article className = "App-Sponsors" id= "Sponsors">
                  <div className="sponsoring"> {/*sponsoring section*/}
                 <font size = "6" color ="white" > {/* title of Sponsors*/}
-              <p class = "right"> SPONSORS </p>
+              <p class = "Sponsor-right"> SPONSORS </p>
                 </font>
 
-            <div className = "row"> {/*made a row for the display*/}
+            <div className = "Sponsor-row"> {/*made a row for the display*/}
                 <div className="column"> {/*named it column instead of top*/}
              <a href= "https://asucm.ucmerced.edu/" > {/*goes to ASUCM site*/}
                 <img className="asucm" src={ASUCM} alt="ASUCM" /> {/*ASUCM logo and link*/}
                  </a> 
                  </div>
-            <div className = "top">
-            <p><a href = "https://digitalocean.com/"> {/*goes to digital ocean site*/}
+            <div className = "Sponsor-top">
+            <a href = "https://digitalocean.com/"> {/*goes to digital ocean site*/}
                  <img className = "digitalocean" src={DigitalOcean} alt="Digital Ocean" /> {/*Digital Ocean logo and link*/}
-            </a></p>
+            </a>
             </div>
 
-            <div className = "top">
+            <div className = "Sponsor-top">
              <a href = "https://roche.com/" > {/*goes to roche site*/}
                  <img className = "roche" src= {roche} alt="Roche"/> {/*Roche logo and link*/}
                  </a>
                  </div>
             </div>
 
-            <div className = "2row"> {/*bottom row for the display*/}
-            <div className = "middle">
+            <div className = "Sponsor-2row"> {/*bottom row for the display*/}
+            <div className = "Sponsor-middle">
                 <div className= "password">
             <a href= "https://1password.com/" > {/*goes to 1 password site*/}
                 <img className= "password" src = {password} alt="1Password"/> {/*1 paswword logo and link*/}
                 </a>
                 </div>
                 
-                <div className = "middle">
+                <div className = "Sponsor-middle">
                     <a href = "https://www.makeschool.com/">
                         <img className = "makeschool" src = {makeschool} alt = "Make School" />
                     </a>
                 </div>
-                <div className = "middle">
+                <div className = "Sponsor-middle">
                     <a href = "https://www.stickeryou.com/">
                         <img className = "stickeryou" src = {stickeryou} alt = "Sticker You" />
                     </a>
                 </div>
                 </div>
             
-                <div className= "3row">
-                <div className = "bottom">
+                <div className= "Sponsor-3row">
+                <div className = "Sponsor-bottom">
             <a href = "https://www.sketch.com/"> {/*goes to sketch site*/}
                  <img className = "sketch" src = {sketch} alt="Sketch" /> {/*Sketch logo and link*/}
             </a>
                 </div>
                
-                 <div className = "bottom">
+                 <div className = "Sponsor-bottom">
                 <a href = "https://balsamiq.com/">
                         <img className = "balsamiq" src = {balsamiq} alt= "Balsamiq" />
             </a>
                     </div>
               
-                <div className = "bottom">
+                <div className = "Sponsor-bottom">
                     <a href = "https://www.jetbrains.com/">
                         <img className = "jetbrains" src = {jetbrains} alt = "Jetbrains" />
                     </a>
                 </div>
                 </div>
                 
-                <div className = "mlh">
+                <div className = "Sponsor-mlh">
                     <a href = "https://mlh.io/">
                         <img className = "mlh" src = {mlh} alt = "Major League Hacking" />
                     </a>
