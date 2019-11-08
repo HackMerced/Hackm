ee0b2bac405145747b22018f8e55f07bc55ccf35
Shivanshu Gupta <shivanshu.gupta@gmail.com>
2019-11-07 16:54:37 -0800

    fixed remote loading of Title page

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import logo from '../../logo.svg';
 import './home.css'; //import css for this page
+import Title from '../../component/Title/Title.jsx'
 
 class Home extends React.Component {   // This page is the exact same page as the original example
   render() {
     return (
       <article className="Home">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <React.Fragment>    {/* This allows children, and removal of unnecessary <div> wrapper */}
+          <Title />
+      </React.Fragment>
       </article>
       );
   }
